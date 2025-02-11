5a0547c72e5c8dcc0eb3ba06dd0dd376147d4b9a
JiangyeLin <linjiangye.im@gmail.com>
2025-02-11 22:52:50 +0800

    删除公司

--- src/main/java/migao/life/visa_manager/controller/CompanyController.java
+++ src/main/java/migao/life/visa_manager/controller/CompanyController.java
@@ -6,10 +6,7 @@ import migao.life.visa_manager.model.entity.CompanyEntity;
 import migao.life.visa_manager.model.form.CompanyQueryForm;
 import migao.life.visa_manager.service.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController()
 @RequestMapping("/company")
@@ -19,13 +16,17 @@ public class CompanyController {
     CompanyService companyService;
 
     @PostMapping("/list")
-    public Result<IPage<CompanyEntity>> getCustomerList( CompanyQueryForm companyQueryForm) {
+    public Result<IPage<CompanyEntity>> getCustomerList(CompanyQueryForm companyQueryForm) {
         return Result.success((companyService.getCompanyList(companyQueryForm)));
     }
 
     @PostMapping("/update")
     public Result<Boolean> saveCompany(@RequestBody CompanyEntity companyEntity) {
-        System.out.println(companyEntity.getId());
         return Result.success(companyService.saveOrUpdate(companyEntity));
+    }
+
+    @DeleteMapping("/delete")
+    public Result<Boolean> delete(@RequestBody CompanyEntity companyEntity) {
+        return Result.success(companyService.removeById(companyEntity));
     }
 }
--- src/main/java/migao/life/visa_manager/model/entity/CompanyEntity.java
+++ src/main/java/migao/life/visa_manager/model/entity/CompanyEntity.java
@@ -1,7 +1,5 @@
 package migao.life.visa_manager.model.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
